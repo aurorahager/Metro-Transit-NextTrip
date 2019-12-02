1ccc1a9d8e03f440de56793ce2dd36b106147d10
Rory Hager <roryy18@gmail.com>
2019-12-02 01:14:18 -0600

    Add styling to search page
    Bring in Semantic UI and Google fonts.
    Add MetroTransit Logo to assets.
    Style search page.

--- src/components/Search/search-view.js
+++ src/components/Search/search-view.js
@@ -1,34 +1,43 @@
 import React from 'react';
+import './Search.css';
+import logo from '../../assets/metro-transit-vector-logo.png';
 
 function SearchView({ routes, directions, stops, handleInputChange, handleSubmit }) {
   return (
-    <div data-test="component-search-view">
-      {/* route select */}
-      <label htmlFor="selectRoute">Select a Route:</label>
-      <select name="route" id="selectRoutes" data-test="search-dropdown-route" onChange={handleInputChange}>
-        <option value={null}>Choose a Route</option>
-        {routes.map(route => (
-          <option key={route.Route} value={route.Route}>{route.Description}</option>
-        ))}
-      </select>
-      {/* direction select */}
-      <label htmlFor="selectDirections">Select a Direction:</label>
-      <select name="direction" id="selectDirections" data-test="search-dropdown-direction" onChange={handleInputChange}>
-        <option value={null}>Choose a Direction</option>
-        {directions.map(direction => (
-          <option key={direction.Value} value={direction.Value}>{direction.Text}</option>
-        ))}
-      </select>
+    <main className="wrapper" data-test="component-search-view">
+      <div className="box aloneBox">
+        <img src={logo} alt="Metro Transit Logo" className="ui large image" />
+      </div>
+      <div className="box aloneBox" >
+        <h1>Let Us Take You <span id="blue">Somewhere</span><span id="red">.</span></h1>
+      </div >
+      <div className="box">
+        {/* route select */}
+        <select aria-label="Select a Route" className="ui selection dropdown" name="route" id="selectRoutes" data-test="search-dropdown-route" onChange={handleInputChange}>
+          <option value={null}>Choose a Route</option>
+          {routes.map(route => (
+            <option key={route.Route} value={route.Route}>{route.Description}</option>
+          ))}
+        </select>
+        {/* direction select */}
+        <select aria-label="Select a Direction" className="ui selection dropdown" name="direction" id="selectDirections" data-test="search-dropdown-direction" onChange={handleInputChange}>
+          <option value={null}>Choose a Direction</option>
+          {directions.map(direction => (
+            <option key={direction.Value} value={direction.Value}>{direction.Text}</option>
+          ))}
+        </select>
+      </div>
       {/* stop select */}
-      <label htmlFor="selectStop">Select a Stop:</label>
-      <select name="stop" id="selectStop" data-test="search-dropdown-stop" onChange={handleInputChange}>
-        <option value={null}>Choose a Stop</option>
-        {stops.map(stop => (
-          <option key={stop.Value} value={stop.Value}>{stop.Text}</option>
-        ))}
-      </select>
-      <button data-test="submit-button" onClick={handleSubmit}>Submit</button>
-    </div >
+      <div className="box bottomBox">
+        <select aria-label="Select a Stop" className="ui selection dropdown" name="stop" id="selectStop" data-test="search-dropdown-stop" onChange={handleInputChange}>
+          <option value={null}>Choose a Stop</option>
+          {stops.map(stop => (
+            <option key={stop.Value} value={stop.Value}>{stop.Text}</option>
+          ))}
+        </select>
+        <button className="ui button" data-test="submit-button" onClick={handleSubmit}>Submit</button>
+      </div>
+    </main >
   );
 };
 
